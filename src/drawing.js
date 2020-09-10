8f7a3d1316437f690fd5e92ff33fee308cacf22a
Jonathan Bowe <bowe.jonathan@yahoo.com>
2020-09-10 19:58:32 -0400

    Working random colors

--- src/drawing.js
+++ src/drawing.js
@@ -6,5 +6,21 @@ sortVis.style = "border:1px #000000;"
 var ctx = sortVis.getContext("2d");
 
 var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+var hash = "#";
+var ranCol = hash.concat(randomColor);
 
-ctx.fillRect(20, 20, 100, 200);
+function drawBar(ctx, upperLeftX, upperLeftY, width, height, color) {
+    ctx.save();
+    ctx.fillStyle = color;
+    ctx.fillRect(upperLeftX, upperLeftY, width, height);
+    ctx.restore();
+}
+
+drawBar(
+    this.ctx,
+    0,
+    0,
+    200,
+    300,
+    ranCol
+);
